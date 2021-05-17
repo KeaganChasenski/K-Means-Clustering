3590ccc0bacb3dfb864c47e2aa904836fa00f8ad
KeaganChasenski <k.chasenski@gmail.com>
2021-05-17 12:17:34 +0200

    inital k=3 set and centriods set as per assignment

--- kmeans.py
+++ kmeans.py
@@ -2,7 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #  Data (examples have two attributes: X, Y , both in range: [1, 10]).
-data = [[2,10],[2,5],[8,4],[5,8],[7,5],[6,4],[1,2],[4,9]]
+data = np.array([[2,10],[2,5],[8,4],[5,8],[7,5],[6,4],[1,2],[4,9]])
+
+#params set by assignment brief
+k = 3
+centroids = np.array([[2,10],[5,8],[1,2]])  
+
+def inital_data_graph():
+    plt.scatter(data[:,0], data[:,1], label = "data points")
+    plt.scatter(centroids[:,0], centroids[:,1], marker="x", label = "centroids")
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Inital data points')
+    plt.legend()
+    plt.savefig('graphs/initial_data.png')
+
 
 if __name__ == "__main__" :
     print("Ran from main")
@@ -10,4 +24,4 @@ if __name__ == "__main__" :
     for i in data:
         print("example ", counter , ":" , i)
         counter += 1 
-
+    inital_data_graph() 
