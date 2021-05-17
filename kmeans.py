d3557f1b50dacf1c41567aeba6a7991250feb638
KeaganChasenski <k.chasenski@gmail.com>
2021-05-17 15:37:02 +0200

    Centroids stored as dictionary for easier indexing

--- kmeans.py
+++ kmeans.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math as math
 
 # Data (examples have two attributes: X, Y , both in range: [1, 10]).
-data = np.array([[2,10],[2,5],[8,4],[5,8],[7,5],[6,4],[1,2],[4,9]])
+X = np.array([[2,10],[2,5],[8,4],[5,8],[7,5],[6,4],[1,2],[4,9]])
 
 # Hyperparamters
 tolerance = 0.001
@@ -10,11 +11,12 @@ max_itterations = 100
 
 # params set by assignment brief
 k = 3
-centroids = np.array([[2,10],[5,8],[1,2]])  
+centroids = {'0': X[0] , '1':X[3], '2':X[6] }
 
+colors = 10*["g","r","c","b","k"]
 
 def inital_data_graph():  
-    plt.scatter(data[:,0], data[:,1], label = "data points")
+    plt.scatter(X[:,0], X[:,1], label = "data points")
     plt.scatter(centroids[:,0], centroids[:,1], marker="x", label = "centroids")
     plt.xlabel('X')
     plt.ylabel('Y')
@@ -23,13 +25,19 @@ def inital_data_graph():
     plt.savefig('graphs/initial_data.png')
 
 def kmeans():
-    print(k,tolerance,max_itterations)
+    print(centroids)
+
+def distance(p1, p2):
+    distance = math.sqrt( ( (p1[0]-p2[0]) **2)  + ( (p1[1]-p2[1]) **2) )
+    return distance
+
 
 if __name__ == "__main__" :
-    print("Ran from main")
-    counter = 1 
-    for i in data:
-        print("example ", counter , ":" , i)
-        counter += 1 
-    inital_data_graph() 
+    #print("Ran from main")
+    #counter = 1 
+    #for i in data:
+        #print("example ", counter , ":" , i)
+        #counter += 1 
+    #inital_data_graph() 
     kmeans()
+    
