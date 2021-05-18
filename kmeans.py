450e4fc2b61fad666776eeaf301d67153f841c5d
KeaganChasenski <k.chasenski@gmail.com>
2021-05-18 11:23:42 +0200

    Was having trouble with using functions at first, and coulnd't debug. Have deleted functions, will aim to get code working without functions and then split into functions after.

--- kmeans.py
+++ kmeans.py
@@ -1,17 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import math as math
+import math
+import copy
 
 # Data (examples have two attributes: X, Y , both in range: [1, 10]).
 X = np.array([[2,10],[2,5],[8,4],[5,8],[7,5],[6,4],[1,2],[4,9]])
 
 # Hyperparamters
 tolerance = 0.001
-max_itterations = 1
+max_itterations = 10
 
 # params set by assignment brief
 k = 3
-centroids = {'0':X[0] , '1':X[3], '2':X[6] }
+centroids = {'1':X[0] , '2':X[3], '3':X[6] }
 
 colors = 10*["g","r","c","b","k"]
 
@@ -37,10 +38,30 @@ def kmeans():
         # Loop for each set of coordinates in data points
         for set in X:
             distances = [distance(set, centroids[centroid]) for centroid in centroids]
-            
             classification = distances.index(min(distances))
+            #print(classification)
             classifications[classification].append(set)
+        print(centroids)
         print(classifications)
+
+        # Compare previous s=centriods 
+        prev_centroids = copy.deepcopy(centroids)
+
+        for classification in classifications:
+            centroids[classification] = np.average(classifications[classification],axis=0)
+            
+        optimzed = True
+
+        for c in centroids:
+            print(c)
+            orginal_centriods = prev_centroids[c+1 + '']
+            current_centriod = centroids[c]
+
+            if np.sum((current_centriod - orginal_centriods) / orginal_centriods * 100) > tolerance:
+                optimzed = False 
+        
+        if optimzed:
+            break 
 
 def distance(p1, p2):
     distance = math.sqrt( ( (p1[0]-p2[0]) **2)  + ( (p1[1]-p2[1]) **2) )
